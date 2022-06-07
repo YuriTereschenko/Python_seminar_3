e26829e2678b3cb1bd6ee4f62a901e69181da379
Yuri Tereschenko <yuri.tereschenko@icloud.com>
2022-06-07 17:33:59 +0500

    task 1 done

---
+++ task1.py
@@ -0,0 +1,31 @@
+# Создать список от -N до N и найти сумму элементов на позициях, позиции указаны в файле
+import random
+
+
+def file_scan(file):  # Считываем данные из файла и переводим их в список целых чисел
+    with open(file, 'r') as pos:
+        position = pos.readlines()
+    for i in range(0, len(position)):
+        position[i] = int(position[i])
+    return position
+
+
+def create_random_array(n, list_of_position):  # Создаем список случайных чисел длинной не меньше максимальной позиции
+    array = []
+    for i in range(0, max(list_of_position)):
+        array.append(random.randint(-n, n))
+    return array
+
+
+def sum_of_nec_el(position, list_for_count):  # Считаем сумму элементов на заданных позициях
+    result = 0
+    for i in position:
+        result += list_for_count[i-1]
+    return result
+
+
+nec_pos = file_scan("position.txt")
+print(f"Позиции из файла:\n{nec_pos}")
+rnd_list = create_random_array(15, nec_pos)
+print(f"Список случайных чисел:\n{rnd_list}")
+print(f"Сумма элементов на заданных позициях = {sum_of_nec_el(nec_pos, rnd_list)}")
